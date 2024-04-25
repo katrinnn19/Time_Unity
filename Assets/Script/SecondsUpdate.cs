c290e520f5ecc000d875ca4bb1dc506e09c83705
katrinnn19 <lgfox941@gmail.com>
2024-04-25 16:03:08 +0200

    Odin_no_ne_11
    11

---
+++ Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    public float speed = 0.5f;
+    float timeStartOffSet = 0;
+    bool gotStartTime = false;
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffSet = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, 
+            this.transform.position.y, 
+            (Time.realtimeSinceStartup - timeStartOffSet) * speed);
+    }
+}
